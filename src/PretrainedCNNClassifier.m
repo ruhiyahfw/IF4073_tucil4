935bcee9d36f5530f374f087820f9ef43b859818
Ruhiyahfw <faradishi@gmail.com>
2022-12-02 21:20:28 +0700

    add laporan+test photo

--- src/PretrainedCNNClassifier.m
+++ src/PretrainedCNNClassifier.m
@@ -1,6 +1,6 @@
 classdef PretrainedCNNClassifier
     methods (Static)
-        function [YPred, score] = predict(image)
+        function YPred = predict(image)
             % load fine-tuned model
             file = load("trained_fruit_classifier_034_042.mat");
             newnet = file.netTransfer;
@@ -10,10 +10,6 @@ classdef PretrainedCNNClassifier
             
             % classify image
             [YPred,scores] = classify(newnet, im);
-
-            % get highest score
-            score = int8(max(scores))*100;
-            score = int2str(score);
         end
     end
 end
